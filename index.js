b6c09d8657e6384b43f52aa7a01594b52714173a
Fernando <fernando.goncalves@aluno.faculdadeimpacta.com.br>
2025-11-11 03:57:34 -0300

    Put and DELETE insert

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import http from "http";
-import {v4} from "uuid";
+import { v4 } from "uuid";
 
 const port = 3000;
 const grades = [];
@@ -7,29 +7,54 @@ const grades = [];
 const server = http.createServer((request, response) => {
   const { method, url } = request;
   let body = "";
+
   request.on("data", (chunk) => {
     body += chunk.toString();
   });
 
   request.on("end", () => {
+    const id = url.split("/")[2];
+
     if (url === "/grades" && method === "GET") {
       response.writeHead(200, { "Content-Type": "application/json" });
       response.end(JSON.stringify(grades));
-
     } else if (url === "/grades" && method === "POST") {
-        const{ studant, subject, grade } = JSON.parse(body);
-        const newGrade = { id: v4(),studant, subject, grade };
-        grades.push(newGrade);
-        response.writeHead(201, { "Content-Type": "application/json" });
-        response.end(JSON.stringify(newGrade));
+      const { studentName, subject, grade } = JSON.parse(body);
+      const newGrade = { id: v4(), studentName, subject, grade };
+      grades.push(newGrade);
+      response.writeHead(201, { "Content-Type": "application/json" });
+      response.end(JSON.stringify(newGrade));
+    } else if (url.startsWith("/grades/") && method === "PUT") {
+      const { studentName, subject, grade } = JSON.parse(body);
+      const gradeToUpdate = grades.find((g) => g.id === id);
+      if (gradeToUpdate) {
+        gradeToUpdate.studentName = studentName;
+        gradeToUpdate.subject = subject;
+        gradeToUpdate.grade = grade;
+        response.writeHead(200, { "Content-Type": "application/json" });
+        response.end(JSON.stringify(gradeToUpdate));
+      } else {
+        response.writeHead(404, { "Content-Type": "application/json" });
+        response.end(JSON.stringify({ message: "Grade not found" }));
+      }
+    } else if (url.startsWith("/grades/") && method === "DELETE") {
+      const index = grades.findIndex((g) => g.id === id);
 
+      if (index !== -1) {
+        grades.splice(index, 1);
+        response.writeHead(204);
+        response.end();
+      } else {
+        response.writeHead(404, { "Content-Type": "application/json" });
+        response.end(JSON.stringify({ message: "Grade not found" }));
+      }
     } else {
       response.writeHead(404, { "Content-Type": "application/json" });
-      response.end(JSON.stringify({ error: "Not Found" }));
+      response.end(JSON.stringify({ message: "Route not found" }));
     }
   });
 });
 
 server.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
